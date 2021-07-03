3672d223d40a647378cd89d073ba3541a254fef5
dillon walsh <dillonwalsh93@gmail.com>
2021-07-03 11:05:56 -0400

    created post note functionality in apiroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,16 +1,38 @@
 const router = require('express').Router();
+const { json } = require('express');
+const fs = require('fs');
 
 // router.get('/test', (req, res) => {
 //     res.send("Heyyyy")
 // })
 
 router.get('/notes', (req, res) => {
-    res.send("hello")
+    var notes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    res.json(notes)
 })
 
 // router.post('/', (req, res) => {
 //     //create new note with an ID 
 // })
+
+router.post('/notes', (req, res) => {
+    console.log("Post",req.body)
+    let newNote = {
+         id: Math.floor(Math.random()*100),
+         title: req.body.title,
+         text: req.body.text
+  
+    }
+    var notes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    // JSON.parse(notes);
+    console.log(notes);
+    notes.push(newNote);
+    fs.writeFileSync("./db/db.json",
+      JSON.stringify(notes),function(err){
+        if (err) throw err;
+      })
+    res.json(notes)
+})
 
 //router.delete('/', (req, res) =>{})
 
